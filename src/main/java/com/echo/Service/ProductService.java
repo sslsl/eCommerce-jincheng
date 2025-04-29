c4f69cac15e0465905ad4cc904c77c1b0f504f5b
Olivia <43398925+sslsl@users.noreply.github.com>
2025-04-28 19:45:35 -0700

    Update ProductService.java

--- src/main/java/com/echo/Service/ProductService.java
+++ src/main/java/com/echo/Service/ProductService.java
@@ -1,7 +1,9 @@
 package com.echo.Service;
 
+import com.echo.models.CategoryEnum;
 import com.echo.models.Product;
 import com.echo.models.ProductDTO;
+import com.echo.models.ProductStatus;
 
 import javax.security.auth.login.LoginException;
 import java.util.List;
@@ -10,4 +12,11 @@ public interface ProductService {
     public Product addProductToCatalog(String token, Product product) throws LoginException;
 
     public List<ProductDTO> getAllProductsOfSeller(Integer id);
+
+    public List<ProductDTO> getProductsOfCategory(CategoryEnum catenum);
+
+    public List<ProductDTO> getProductsOfStatus(ProductStatus status);
+
+    public Product updateProductQuantityWithId(Integer id,ProductDTO prodDTO);
+
 }
