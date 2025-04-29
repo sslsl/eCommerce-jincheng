e8d0ca08260811f66b38ea39c4408bc81a0b8534
Olivia <43398925+sslsl@users.noreply.github.com>
2025-04-28 19:36:51 -0700

    Update OrderDao.java

--- src/main/java/com/echo/repository/OrderDao.java
+++ src/main/java/com/echo/repository/OrderDao.java
@@ -2,16 +2,22 @@ package com.echo.repository;
 
 import com.echo.models.Customer;
 import com.echo.models.Order;
+import com.echo.models.OrderStatusValues;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 
 @Repository
 public interface OrderDao extends JpaRepository<Order, Integer> {
 
-//    @Query("select * from Customer c where c.customerId = customerId")
-//    public Customer getCustomerByOrderId(int customerId);
-
+    @Modifying
+    @Transactional
+    @Query("UPDATE Order o SET o.orderStatus = :status WHERE o.orderId = :orderId")
+    public int updateOrderStatusById(@Param("orderId") Integer orderId, @Param("status") OrderStatusValues status);
 
 }
